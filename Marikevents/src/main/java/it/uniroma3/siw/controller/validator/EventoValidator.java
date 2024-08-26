0317fc8b83276c258ead64b50aca3006e7b7175b
Sopbaldini2 <sop.baldini@stud.uniroma3.it>
2024-08-26 20:24:48 +0200

    Riguarda le recensioni

---
+++ Marikevents/src/main/java/it/uniroma3/siw/controller/validator/EventoValidator.java
@@ -0,0 +1,28 @@
+package it.uniroma3.siw.controller.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import it.uniroma3.siw.model.Evento;
+import it.uniroma3.siw.repository.EventoRepository;
+
+@Component
+public class EventoValidator implements Validator {
+	@Autowired
+	private EventoRepository eventoRepository;
+
+	@Override
+	public void validate(Object o, Errors errors) {
+		Evento evento = (Evento)o;
+		if (evento.getNome()!=null && evento.getData()!=null 
+				&& eventoRepository.existsByNomeIgnoreCaseAndData(evento.getNome(), evento.getData())) {
+			errors.reject("evento.duplicate");
+		}
+	}
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return Evento.class.equals(aClass);
+	}
+}
---
+++ Marikevents/src/main/java/it/uniroma3/siw/repository/EventoRepository.java
@@ -0,0 +1,26 @@
+package it.uniroma3.siw.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import it.uniroma3.siw.model.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento,Long>{
+	
+	public List<Evento> findByNome(String nome);
+	
+	boolean existsByNomeIgnoreCaseAndData(String nome, LocalDate data);
+	
+	@Modifying
+    @Query(value = "delete from recensione WHERE evento_id = :eventoId", nativeQuery=true)
+    void deleteReferencesInRecensione(@Param("eventoId") Long eventoId);
+
+	@Query("select e from Evento e ORDER BY e.nome")
+	public List<Evento> findAllSortedByNome();
+
+}
